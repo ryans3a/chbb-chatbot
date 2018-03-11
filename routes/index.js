8f7d6c719708abda20851d6c147ef1327bba41b4
James Gu <james_gu14@hotmail.com>
2018-03-11 13:55:07 +0800

    integrate intent service

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
-var express = require('express');
-var router = express.Router();
+'use strict'
+
+const express = require('express')
+const router = express.Router()
+const test = require('../services/test')
+const textSementic = require('../services/textSementic')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' })
+})
 
-module.exports = router;
+router.get('/test', function(req, res) {
+
+  test(function(body) {
+    res.send('finish')
+  })
+})
+
+module.exports = router
