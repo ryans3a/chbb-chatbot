8222959732a548dad73ad37ba7e9637f9b9da6f3
James Gu <james_gu14@hotmail.com>
2018-03-19 16:20:21 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,17 @@
 'use strict'
 
 const express = require('express')
+const path = require('path')
+const fs = require('fs')
 const router = express.Router()
-const textSementicService = require('../services/aiui/textSementic')
+const textSemanticService = require('../services/aiui/textSementic')
+const audioSemanticService = require('../services/aiui/audioSementic')
 const logQuery = require('../services/logService').logQuery
 const parseResp = require('../services/funcs/').parseResp
+const multer = require('multer')
+const upload = multer({
+  dest: 'public/audio/query'
+})
 
 /* GET home page. */
 router.get('/', function (req, res) {
@@ -18,7 +25,7 @@ router.get('/textSementic', function (req, res) {
   let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
   let query = req.query.q
 
-  textSementicService(query).then((data) => {
+  textSemanticService(query).then((data) => {
 
     // Process resp
     let resp = JSON.parse(data).data
@@ -45,11 +52,58 @@ router.get('/textSementic', function (req, res) {
   })
 })
 
-router.get('/audioSementic', function (req, res) {
+router.post('/audioSementic', upload.single('file'), function (req, res) {
+
+  let mimeType = req.file.mimetype
+  if (!mimeType.startsWith('audio')) {
+    return res.send('不是Audio文件')
+  }
+
+  let maxSize = 1 * 1000 * 1000 // 300kb max
+  if (req.file.size > maxSize) {
+    return res.send('上传失败，文件超过1MB')
+  }
+
+  // let filename = req.file.filename
+  let filepath = req.file.path
+  let originalName = req.file.originalname
+  let postfix = originalName.split('.')[1]
+  let newPath = filepath + '.' + postfix
+  
+  // let uploadPath = path.join(__dirname, '../public/audio/query')
+
+  fs.renameSync(filepath, newPath)
 
   let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
   let query = req.query.q
 
+  audioSemanticService(newPath).then((data) => {
+
+    // Process resp
+    let resp = JSON.parse(data).data
+
+    parseResp(resp).then((answer) => {
+
+        logQuery(query, ip, true).then(() => {
+
+          res.json(answer)
+        })
+      })
+      .catch((err) => {
+
+        logQuery(query, ip, false).then(() => {
+          
+          res.send(err)
+        })
+      })
+  }).catch((err) => {
+    logQuery(query, ip, false)
+    return res.json({
+      'error': err
+    })
+  })
+
+  res.send('finish')
 })
 
 router.get('/test', function (req, res) {
